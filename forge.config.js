0f8616001a6309722ea0a6a05ab56f0c462bed87
Spencer Stewart <spencerjamesstewart@gmail.com>
2024-01-16 20:53:22 -0600

    ci(project): use electron-builder to build windows executable

--- forge.config.js
+++ forge.config.js
@@ -5,19 +5,8 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: "@electron-forge/maker-squirrel",
-      config: {
-        name: "price-check-app",
-      },
-    },
-    {
       name: "@electron-forge/maker-zip",
       platforms: ["darwin"],
-    },
-    {
-      // Add maker for Windows zip if you plan to have a portable version
-      name: "@electron-forge/maker-zip",
-      platforms: ["win32"],
     },
   ],
   plugins: [
